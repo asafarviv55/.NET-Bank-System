9e9ea968758144e0b43371835c844e37f81c4d8f
asafarviv55 <asaf.arviv@gmail.com>
2022-07-14 18:21:35 +0300

    4

---
+++ Bank/Data/DbInitializer.cs
@@ -0,0 +1,42 @@
+﻿using Bank.Models;
+
+namespace Bank.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(BankContext context)
+        {
+            // Look for any Users.
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var users = new User[]
+            {
+                new User{}
+
+            };
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+
+            /*********************************/
+            // Look for any students.
+            if (context.UsersAuths.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var usersAuths = new UsersAuth[]
+            {
+                new UsersAuth{owner}
+
+            };
+
+            context.UsersAuths.AddRange(usersAuths);
+            context.SaveChanges();
+        }
+    }
+}
