a8b0e76c81498b9f6891eb58579bb5ce86e813dc
asafarviv55 <asaf.arviv@gmail.com>
2022-07-14 18:05:38 +0300

    second push

---
+++ Bank/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bank.Models
+{
+    public class User
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int id { get; set; }
+
+        public User owner { get; set; }
+
+
+
+
+
+
+
+    }
+}
---
+++ Bank/Models/UsersAuth.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bank.Models
+{
+    public class UsersAuth
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int id { get; set; }
+
+        public User owner { get; set; }
+
+
+
+
+    }
+}
