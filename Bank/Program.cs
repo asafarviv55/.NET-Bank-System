18729777f5572b74715e26843e7b70090f0fa224
asafarviv55 <asaf.arviv@gmail.com>
2022-07-21 16:39:22 +0300

    add External Login Google

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -1,9 +1,9 @@
-global using Bank.Models;
 global using Bank.Data;
+global using Bank.Models;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -11,12 +11,18 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<BankContext>(options =>
   options.UseSqlServer(builder.Configuration.GetConnectionString("BankContext")));
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+        options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<BankContext>();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-
+builder.Services.AddAuthentication()
+   .AddGoogle(options =>
+   {
+       options.ClientId = "720891221756-q84ml3nmqubaftqobuhu381hini4ct6a.apps.googleusercontent.com";
+       options.ClientSecret = "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";
+   });
 
 var app = builder.Build();
 
@@ -48,7 +54,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication(); ;
+app.UseAuthentication();
 
 app.UseAuthorization();
 
