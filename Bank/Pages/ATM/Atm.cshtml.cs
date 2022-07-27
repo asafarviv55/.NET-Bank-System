30a9beaaa3698c6663a87db93d91073871e2c90e
asaf arviv <asaf.arviv@gmail.com>
2022-07-27 20:44:59 +0200

    add ATM Screen

---
+++ Bank/Pages/ATM/Atm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bank.Pages.ATM
+{
+    public class AtmModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
