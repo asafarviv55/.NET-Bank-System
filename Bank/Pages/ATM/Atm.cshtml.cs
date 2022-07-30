abdeb239be7a1e3293cb731f0a3718fa9d056c26
asaf arviv <asaf.arviv@gmail.com>
2022-07-31 00:57:24 +0300

    add logger

--- Bank/Pages/ATM/Atm.cshtml.cs
+++ Bank/Pages/ATM/Atm.cshtml.cs
@@ -5,6 +5,14 @@ namespace Bank.Pages.ATM
 {
     public class AtmModel : PageModel
     {
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public AtmModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
         }
